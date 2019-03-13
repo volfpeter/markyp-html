649687b36ee7aa21e76a3947e6f2fee99ca1c0aa
Péter Volf <peter@milinnovations.com>
2019-03-13 12:18:27 +0100

    added the headings module that contains the <h{n}> elements

---
+++ src/markyp_html/headings.py
@@ -0,0 +1,59 @@
+"""
+HTML heading elements.
+"""
+
+from markyp.elements import Element
+
+
+class h1(Element):
+    """
+    `<h1></h1>` element.
+
+    See https://www.w3schools.com/tags/tag_hn.asp.
+    """
+    __slots__ = ()
+
+
+class h2(Element):
+    """
+    `<h2></h2>` element.
+
+    See https://www.w3schools.com/tags/tag_hn.asp.
+    """
+    __slots__ = ()
+
+
+class h3(Element):
+    """
+    `<h3></h3>` element.
+
+    See https://www.w3schools.com/tags/tag_hn.asp.
+    """
+    __slots__ = ()
+
+
+class h4(Element):
+    """
+    `<h4></h4>` element.
+
+    See https://www.w3schools.com/tags/tag_hn.asp.
+    """
+    __slots__ = ()
+
+
+class h5(Element):
+    """
+    `<h5></h5>` element.
+
+    See https://www.w3schools.com/tags/tag_hn.asp.
+    """
+    __slots__ = ()
+
+
+class h6(Element):
+    """
+    `<h6></h6>` element.
+
+    See https://www.w3schools.com/tags/tag_hn.asp.
+    """
+    __slots__ = ()
